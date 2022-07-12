952ca6fa814d25cf579687e9d9d64b0a73b10348
tiffaniero <tiffanie.roger01@gmail.com>
2022-07-12 16:33:40 +0200

    2nd commit

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
+        for (int i = 0; i < 7; i++) {
             System.out.println(i + " tour de boucle");
         }
     }
